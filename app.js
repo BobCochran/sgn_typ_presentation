6a774a8f336796181707520d2259fffee618a21d
BobCochran <r2cochran2@gmail.com>
2014-02-27 19:44:18 -0500

    Add the first bits of node.js http server code. At this point I am following the WebStorm webinar presentation by Adron Hall for managing a Node.js application in WebStorm.

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 /**
  * Created by Bob Cochran on 2014-02-27.
+ * This will be the main SignTyp Imaging Project application file. The database backend is a separate
+ * project, sgntyp_database.
  *
  */
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
