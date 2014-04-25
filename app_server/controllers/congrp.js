28491a8a92cced8cebb0ccd164f56d81a871d4e8
BobCochran <r2cochran2@gmail.com>
2014-04-24 20:02:54 -0400

    Begin displaying list of concept groups -- fixing header first of all.

--- app_server/controllers/congrp.js
+++ app_server/controllers/congrp.js
@@ -1,6 +1,6 @@
 /* Get 'home' page */
 module.exports.homelist = function(req, res) {
-    res.render('index', { title: 'Home'} )
+    res.render('index', { title: 'Signtyp Imaging Project'} )
 }
 
 /* GET 'concept group' page */
