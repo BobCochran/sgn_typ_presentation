96b83b475e256797a29bbec85d1dc63066104347
BobCochran <r2cochran2@gmail.com>
2014-04-26 17:06:09 -0400

    Begin displaying list of concept groups -- sixth attempt to list groups. Using new app2.js and CongroupProvider2.js.

--- app_server/controllers/congrp.js
+++ app_server/controllers/congrp.js
@@ -1,8 +1,11 @@
-var CongroupProvider2 = require('../models/CongroupProvider2').CongroupProvider2
+CongroupProvider2 = require('../models/CongroupProvider2').CongroupProvider2
 
 /* Get 'home' page */
 module.exports.homelist = function(req, res) {
-    CongroupProvider2.getCongroups(function (err,results) {
+
+    var cgroups = new CongroupProvider2(db)
+
+    cgroups.getCongroups(function (err,results) {
         if (err) return err
         res.render('index', {
             title: 'Signtyp Imaging Project Concept Groups',
